8f826a13c8f412a7f29218d67632e64b93225ead
David Pang <davidpang314@gmail.com>
2021-04-28 15:05:59 -0400

    Started from scratch on new website.

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function () {
+    $(function () {
+        $(document).scroll(function () {
+          var $nav = $("nav");
+          $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        });
+    });
+});
